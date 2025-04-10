66d1c1801945bb9726fc462c127730a7253f8d33
naeimsalib <naeimsalib@yahoo.com>
2025-04-09 20:52:58 -0400

    Update app to upload on heroku

--- run.py
+++ run.py
@@ -2,16 +2,22 @@ import os
 from dotenv import load_dotenv
 from job_app_tracker import create_app
 
+# Load environment variables
+load_dotenv()
+
 # Create the Flask app
 app = create_app()
 
 if __name__ == '__main__':
-    # Fetch port from environment variable or use 3000 as default
-    port = int(os.environ.get('PORT', 3000))
+    # Fetch port from environment variable or use 5000 as default
+    port = int(os.environ.get('PORT', 5000))
+    
+    # Set debug mode based on environment
+    debug = os.environ.get('FLASK_ENV') == 'development'
     
     # Run the app
     app.run(
         host='0.0.0.0',
         port=port,
-        debug=True
+        debug=debug
     ) 
